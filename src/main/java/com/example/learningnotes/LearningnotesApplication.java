3f03936e99341680b0272d9092409cd3012c5ec9
YangChen <1763318483@qq.com>
2022-06-14 10:01:26 +0800

    indexes失效

---
+++ src/main/java/com/example/learningnotes/LearningnotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningnotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningnotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learningnotes/LearningnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningnotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
